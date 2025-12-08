a03e5f0837986c8e30e86ebf7b80d6fb1fccaca3
Chyiyin Gwee <46178076+kakapocy@users.noreply.github.com>
2025-12-08 15:38:09 +0100

    Update 1.0.5_eagerTEbam.sh

--- Analyses/1.0.5_eagerTEbam.sh
+++ Analyses/1.0.5_eagerTEbam.sh
@@ -1,28 +1,24 @@
 #!/bin/bash -l
 #SBATCH -J eagerTEbam
-#SBATCH --get-user-env
-#SBATCH --mail-user=gwee@biologie.uni-muenchen.de
-#SBATCH --clusters=biohpc_gen
-#SBATCH --partition=biohpc_gen_normal
 #SBATCH --cpus-per-task=12
 #SBATCH --time=2-00:00:00
-#SBATCH -o /dss/dsslegfs01/pr53da/pr53da-dss-0018/projects/2020__ancientDNA/05_aDNA/slurms/slurm-%j-%x.out
+#SBATCH -o PATH/05_aDNA/slurms/slurm-%j-%x.out
 
-# sbatch 1.0.5_eagerTEbam.sh 00_eager_mybaits /dss/dsslegfs01/pr53da/pr53da-dss-0018/projects/2020__ancientDNA/05_aDNA/genome_HC_allpaths41687_v2.5.fasta /dss/dsslegfs01/pr53da/pr53da-dss-0018/projects/2020__ancientDNA/05_aDNA/acrow_eager_list_mybaits_bam.tsv probes_232015_SNPsite
-# sbatch 1.0.5_eagerTEbam.sh 00_eager_twist /dss/dsslegfs01/pr53da/pr53da-dss-0018/projects/2020__ancientDNA/05_aDNA/genome_HC_allpaths41687_v2.5.fasta /dss/dsslegfs01/pr53da/pr53da-dss-0018/projects/2020__ancientDNA/05_aDNA/acrow_eager_list_twist_bam.tsv probes_232015_SNPsite
-# sbatch 1.0.5_eagerTEbam.sh 00_eager_enrichedall /dss/dsslegfs01/pr53da/pr53da-dss-0018/projects/2020__ancientDNA/05_aDNA/genome_HC_allpaths41687_v2.5.fasta /dss/dsslegfs01/pr53da/pr53da-dss-0018/projects/2020__ancientDNA/05_aDNA/acrow_eager_list_enrichedall_bam.tsv probes_232015_SNPsite
+# sbatch 1.0.5_eagerTEbam.sh 00_eager_mybaits PATH/05_aDNA/genome_HC_allpaths41687_v2.5.fasta PATH/05_aDNA/acrow_eager_list_mybaits_bam.tsv probes_232015_SNPsite
+# sbatch 1.0.5_eagerTEbam.sh 00_eager_twist PATH/05_aDNA/genome_HC_allpaths41687_v2.5.fasta PATH/05_aDNA/acrow_eager_list_twist_bam.tsv probes_232015_SNPsite
+# sbatch 1.0.5_eagerTEbam.sh 00_eager_enrichedall PATH/05_aDNA/genome_HC_allpaths41687_v2.5.fasta PATH/05_aDNA/acrow_eager_list_enrichedall_bam.tsv probes_232015_SNPsite
 
-# sbatch 1.0.5_eagerTEbam.sh 00_eager_mybaits /dss/dsslegfs01/pr53da/pr53da-dss-0018/projects/2020__ancientDNA/05_aDNA/genome_HC_allpaths41687_v2.5.fasta /dss/dsslegfs01/pr53da/pr53da-dss-0018/projects/2020__ancientDNA/05_aDNA/acrow_eager_list_mybaits_bam.tsv probes_104k
-# sbatch 1.0.5_eagerTEbam.sh 00_eager_twist /dss/dsslegfs01/pr53da/pr53da-dss-0018/projects/2020__ancientDNA/05_aDNA/genome_HC_allpaths41687_v2.5.fasta /dss/dsslegfs01/pr53da/pr53da-dss-0018/projects/2020__ancientDNA/05_aDNA/acrow_eager_list_twist_bam.tsv probes_104k_80bp
-# sbatch 1.0.5_eagerTEbam.sh 00_eager_mybaits /dss/dsslegfs01/pr53da/pr53da-dss-0018/projects/2020__ancientDNA/05_aDNA/genome_HC_allpaths41687_v2.5.fasta /dss/dsslegfs01/pr53da/pr53da-dss-0018/projects/2020__ancientDNA/05_aDNA/acrow_eager_list_mybaits_bam.tsv probes_104k_80bp 
+# sbatch 1.0.5_eagerTEbam.sh 00_eager_mybaits PATH/05_aDNA/genome_HC_allpaths41687_v2.5.fasta PATH/05_aDNA/acrow_eager_list_mybaits_bam.tsv probes_104k
+# sbatch 1.0.5_eagerTEbam.sh 00_eager_twist PATH/05_aDNA/genome_HC_allpaths41687_v2.5.fasta PATH/05_aDNA/acrow_eager_list_twist_bam.tsv probes_104k_80bp
+# sbatch 1.0.5_eagerTEbam.sh 00_eager_mybaits PATH/05_aDNA/genome_HC_allpaths41687_v2.5.fasta PATH/05_aDNA/acrow_eager_list_mybaits_bam.tsv probes_104k_80bp 
 
-# sbatch 1.0.5_eagerTEbam.sh 00_eager_twist /dss/dsslegfs01/pr53da/pr53da-dss-0018/projects/2020__ancientDNA/05_aDNA/genome_HC_allpaths41687_v2.5.fasta /dss/dsslegfs01/pr53da/pr53da-dss-0018/projects/2020__ancientDNA/05_aDNA/acrow_eager_list_twist_bam.tsv probes_104k_SNPsite
-# sbatch 1.0.5_eagerTEbam.sh 00_eager_mybaits /dss/dsslegfs01/pr53da/pr53da-dss-0018/projects/2020__ancientDNA/05_aDNA/genome_HC_allpaths41687_v2.5.fasta /dss/dsslegfs01/pr53da/pr53da-dss-0018/projects/2020__ancientDNA/05_aDNA/acrow_eager_list_mybaits_bam.tsv probes_104k_SNPsite
+# sbatch 1.0.5_eagerTEbam.sh 00_eager_twist PATH/05_aDNA/genome_HC_allpaths41687_v2.5.fasta PATH/05_aDNA/acrow_eager_list_twist_bam.tsv probes_104k_SNPsite
+# sbatch 1.0.5_eagerTEbam.sh 00_eager_mybaits PATH/05_aDNA/genome_HC_allpaths41687_v2.5.fasta PATH/05_aDNA/acrow_eager_list_mybaits_bam.tsv probes_104k_SNPsite
 
-# sbatch 1.0.5_eagerTEbam.sh 00_eager_twist /dss/dsslegfs01/pr53da/pr53da-dss-0018/projects/2020__ancientDNA/05_aDNA/genome_HC_allpaths41687_v2.5.fasta /dss/dsslegfs01/pr53da/pr53da-dss-0018/projects/2020__ancientDNA/05_aDNA/acrow_eager_list_twist_bam.tsv probes_232015
+# sbatch 1.0.5_eagerTEbam.sh 00_eager_twist PATH/05_aDNA/genome_HC_allpaths41687_v2.5.fasta PATH/05_aDNA/acrow_eager_list_twist_bam.tsv probes_232015
 
-# sbatch 1.0.5_eagerTEbam.sh 00_eager_mybaits /dss/dsslegfs01/pr53da/pr53da-dss-0018/projects/2020__ancientDNA/05_aDNA/genome_HC_allpaths41687_v2.5.fasta /dss/dsslegfs01/pr53da/pr53da-dss-0018/projects/2020__ancientDNA/05_aDNA/acrow_eager_list_mybaits_bam.tsv probes_232015
-# sbatch 1.0.5_eagerTEbam.sh 00_eager_twist /dss/dsslegfs01/pr53da/pr53da-dss-0018/projects/2020__ancientDNA/05_aDNA/genome_HC_allpaths41687_v2.5.fasta /dss/dsslegfs01/pr53da/pr53da-dss-0018/projects/2020__ancientDNA/05_aDNA/acrow_eager_list_twist_bam.tsv probes_104k
+# sbatch 1.0.5_eagerTEbam.sh 00_eager_mybaits PATH/05_aDNA/genome_HC_allpaths41687_v2.5.fasta PATH/05_aDNA/acrow_eager_list_mybaits_bam.tsv probes_232015
+# sbatch 1.0.5_eagerTEbam.sh 00_eager_twist PATH/05_aDNA/genome_HC_allpaths41687_v2.5.fasta PATH/05_aDNA/acrow_eager_list_twist_bam.tsv probes_104k
 
 echo $(date)
 STARTTIME=$(date +%s)
@@ -32,7 +28,7 @@ STARTTIME=$(date +%s)
 module load nextflow
 conda activate biotools
 
-dat="/dss/dsslegfs01/pr53da/pr53da-dss-0018/projects/2020__ancientDNA/05_aDNA"
+dat="PATH/05_aDNA"
 
 cd ${dat}
 mkdir ${1}
@@ -41,15 +37,15 @@ cd $1
 #with snpsite bed files and trimmed bam
 unset DISPLAY
 nextflow run nf-core/eager -profile conda -r 2.4.7 --input ${3} --fasta ${2} \
--c /dss/dsslegfs01/pr53da/pr53da-dss-0018/projects/2020__ancientDNA/05_aDNA/base_modified2.config \
---snpcapture_bed /dss/dsslegfs01/pr53da/pr53da-dss-0018/projects/2020__ancientDNA/05_aDNA/${4}.bed \
+-c PATH/05_aDNA/base_modified2.config \
+--snpcapture_bed PATH/05_aDNA/${4}.bed \
 --mtnucratio_header chrM --run_mtnucratio \
 --run_trim_bam --bamutils_clip_single_stranded_none_udg_left 5 --bamutils_clip_single_stranded_none_udg_right 5 
 
 # include duplicates
 nextflow run nf-core/eager -profile conda -r 2.4.7 --input ${3} --fasta ${2} \
--c /dss/dsslegfs01/pr53da/pr53da-dss-0018/projects/2020__ancientDNA/05_aDNA/base_modified2.config \
---snpcapture_bed /dss/dsslegfs01/pr53da/pr53da-dss-0018/projects/2020__ancientDNA/05_aDNA/${4}.bed \
+-c PATH/05_aDNA/base_modified2.config \
+--snpcapture_bed PATH/05_aDNA/${4}.bed \
 --skip_preseq --skip_deduplication --skip_damage_calculation \
 --run_trim_bam --bamutils_clip_single_stranded_none_udg_left 5 --bamutils_clip_single_stranded_none_udg_right 5
 
