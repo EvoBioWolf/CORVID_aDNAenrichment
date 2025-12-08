1863709a1b384223a9c4d33b492728b1ed59a817
Chyiyin Gwee <46178076+kakapocy@users.noreply.github.com>
2025-12-08 15:31:40 +0100

    Update 2.0_outgroup.sh

--- SNP_panel_2/2.0_outgroup.sh
+++ SNP_panel_2/2.0_outgroup.sh
@@ -1,12 +1,8 @@
 #!/bin/bash -l
 #SBATCH -J outgroup
-#SBATCH --get-user-env
-#SBATCH --mail-user=gwee@biologie.uni-muenchen.de
-#SBATCH --clusters=biohpc_gen
-#SBATCH --partition=biohpc_gen_normal
 #SBATCH --cpus-per-task=6
 #SBATCH --time=2-00:00:00
-#SBATCH -o /dss/dsslegfs01/pr53da/pr53da-dss-0018/projects/2020__ancientDNA/01_probes/snp_panel_2/slurms/slurm-%j-%x.out
+#SBATCH -o PATH/01_probes/snp_panel_2/slurms/slurm-%j-%x.out
 #SBATCH --mem-per-cpu=4763mb
 
 sbatch 2.0_outgroup.sh 134inds_overlapped_filtered_norepeats_AMcrow_biallele 9pop outgroup_ascertained3
@@ -16,15 +12,15 @@ module load vcftools/0.1.14-gcc8
 module load bcftools
 #picard.jar=2.25.7
 
-ref="/dss/dsslegfs01/pr53da/pr53da-dss-0018/projects/2020__ancientDNA/04_fresh2/genome_HC_allpaths41687_v2.5_chrW.fasta"
-dat="/dss/dsslegfs01/pr53da/pr53da-dss-0018/projects/2020__ancientDNA/01_probes/snp_panel_2"
-neu="/dss/dsslegfs01/pr53da/pr53da-dss-0018/projects/2020__ancientDNA/01_probes/neutral"
+ref="PATH/04_fresh2/genome_HC_allpaths41687_v2.5_chrW.fasta"
+dat="PATH/01_probes/snp_panel_2"
+neu="PATH/01_probes/neutral"
 
 echo $(date)
 STARTTIME=$(date +%s)
 
 cd ${dat}/$3
-vcftools --keep ${dat}/poplist/${2}.txt --gzvcf /dss/dsslegfs01/pr53da/pr53da-dss-0018/projects/2020__ancientDNA/04_fresh2/05.1_recal/overlap/${1}.vcf.gz --recode --out ${2}_overlapped_filtered_norepeats_AMcrow_biallele
+vcftools --keep ${dat}/poplist/${2}.txt --gzvcf PATH/04_fresh2/05.1_recal/overlap/${1}.vcf.gz --recode --out ${2}_overlapped_filtered_norepeats_AMcrow_biallele
 bcftools view -Oz -o ${2}_overlapped_filtered_norepeats_AMcrow_biallele.vcf.gz ${2}_overlapped_filtered_norepeats_AMcrow_biallele.recode.vcf
 bcftools index ${2}_overlapped_filtered_norepeats_AMcrow_biallele.vcf.gz
 rm ${2}_overlapped_filtered_norepeats_AMcrow_biallele.recode.vcf
